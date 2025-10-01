a6a825524aa2d3ac02bc950bb46c497e892510e4
pero4l <lilpero03@gmail.com>
2025-10-01 12:50:19 +0100

    added a dotenv and gitignore file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 
-const PORT = 9000
+
+app.use(express.json())
 
 app.get('/', (req, res) =>{
     res.status(200).json({
@@ -10,7 +12,9 @@ app.get('/', (req, res) =>{
     })
 })
 
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
     console.log(`Server running on PORT:${PORT}`);
     
-})
+}) 
