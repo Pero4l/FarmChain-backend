cd66cbe950ece34a91176b95c385968c0c46e5f1
pero4l <lilpero03@gmail.com>
2025-10-01 22:54:50 +0100

    finished writing the register function and want to route it now

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,7 +25,48 @@ async function register(req, res) {
     } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
         return res.status(400).json({message: "Invalid email format"});
 
-    } else if (name.length < 3) {
+    } else if (firstName.length < 3 || lastName.length < 3) {
         return res.status(400).json({message: "Name must be at least 3 characters"});
     }
+
+    if(data['users'].find((u)=> u.email === email)){
+         return res.status(400).json({
+            "success": false,
+            "message": "User already exists"
+        })
+    }
+
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+    const id = data['users'].length + 1;
+    const createdAt = new Date().toLocaleString();
+
+    const newUser = {
+        id,
+        firstName,
+        lastName, 
+        email, 
+        phone, 
+        address, 
+        state, 
+        country, 
+        password: hashedPassword, 
+        createdAt, 
+        userProfile: {}, 
+        notifications: []}
+
+
+    newUser['notifications'].push({notification:`Account created successfully`})
+
+    data['users'].push(newUser)
+    writeDb(data)
+
+    res.status(201).json({ 
+        "success" : true,
+        "message": "Account registered successfully",
+        "data": newUser
+    });
 }
+
+
+module.exports = {register}
