23761fb7192a3117c348ff5f808f04e02b5274c6
pero4l <lilpero03@gmail.com>
2025-10-02 20:59:04 +0100

    i just wrote the controller for login

---
+++ middleware/loginMiddleware.js
@@ -0,0 +1,33 @@
+const {readDb} = require('../utils/dbOperation')
+const bcrypt = require('bcrypt')
+
+async function loginMiddleware(req, res, next){
+    const {email, password} = req.body
+
+    const data = readDb()
+    const existUser = data['users'].find((u) => u.email === email)
+
+    if (!existUser) {
+        return res.status(404).json({
+            success: false,
+            message: "User does not exist"
+        });
+    }
+
+    const passMatch = await bcrypt.compare(password, existUser.password)
+     if (!passMatch) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid credentials"
+        });
+    }
+
+    req.user = passMatch
+    req.data = existUser
+    next()
+}
+
+
+module.exports = {
+    loginMiddleware
+}
