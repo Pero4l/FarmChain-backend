bce7eb78e602816bc75b9158650505702529b818
pero4l <lilpero03@gmail.com>
2025-10-13 23:00:53 +0100

    writing to add content

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,5 +2,15 @@ const {readDb, writeDb} = require('../utils/dbOperation')
 
 async function newsFeedPost(req, res) {
     const{content} = req.body
+
+    
+    
+    if(!content){
+        return res.status(400).json({
+            "sucess": false,
+            "message": "All field are required"
+        })
+    }
+
     
 }
