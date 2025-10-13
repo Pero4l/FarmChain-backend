1c5a7f3d716290ea4e398eb933243e3178cc5b08
pero4l <lilpero03@gmail.com>
2025-10-13 23:15:24 +0100

    getting user location

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,10 +72,11 @@ async function register(req, res) {
 
 async function login(req, res) {
 
-    const token = jwt.sign({userId: req.data.id, currentUser: req.data.name}, process.env.JWT_SECRET, {expiresIn: '5h'})
+    const token = jwt.sign({userId: req.data.id, currentUser: req.data.name, location: req.data.location}, process.env.JWT_SECRET, {expiresIn: '5h'})
 
     const userId = req.data.id
     const currentUser = req.data.name
+    const location = req.data.location
     
     if(req.user){
         return res.status(200).json({
@@ -83,7 +84,8 @@ async function login(req, res) {
         "message": "Login Successfully",
         "token": token,
         "userId": userId,
-        "currentUser": currentUser
+        "currentUser": currentUser,
+        "location": location
     })
     }
     
