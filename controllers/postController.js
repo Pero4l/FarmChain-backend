08527d407272792a53ada67277b855e8b967f51a
pero4l <lilpero03@gmail.com>
2025-10-13 23:35:03 +0100

    collecting data

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,5 +14,22 @@ async function newsFeedPost(req, res) {
 
     const existPost = data['posts'].find((p) => p.content === content)
 
+    if(existPost){
+        return res.status(400).json({
+            "sucess": false,
+            "message": "Post already exist"
+        })
+    }
+
+    const user = req.user
+    const id = data['posts'].length + 1
+    const farm = user.name
+    const location = user.location
+    const avatar = user.id
     
+
+    const newPost = {
+
+    }
+
 }
