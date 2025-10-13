c7757a370a46d0f047305d64abc9afacfb8d7d7b
pero4l <lilpero03@gmail.com>
2025-10-13 23:01:42 +0100

    still on it

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,7 +3,7 @@ const {readDb, writeDb} = require('../utils/dbOperation')
 async function newsFeedPost(req, res) {
     const{content} = req.body
 
-    
+    const data = readDb()
     
     if(!content){
         return res.status(400).json({
