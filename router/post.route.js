ded984e4c7b45a5a83b219d35d245036a3167e0b
pero4l <lilpero03@gmail.com>
2025-10-16 23:28:34 +0100

    routed it and want to test it

--- router/post.route.js
+++ router/post.route.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const {authMiddleware} = require('../middleware/authUserMiddleware')
-const {newsFeedPost} = require('../controllers/postController')
+const {newsFeedPost, getAllPost} = require('../controllers/postController')
 
 router.post('/create', authMiddleware, newsFeedPost)
+router.get('/', authMiddleware, getAllPost)
 
 module.exports = router
