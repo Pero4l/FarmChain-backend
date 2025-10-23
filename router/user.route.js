c4ec03c719ee486a6d5aa0028ce8ef67521d8780
pero4l <lilpero03@gmail.com>
2025-10-23 23:12:32 +0100

    still

--- router/user.route.js
+++ router/user.route.js
@@ -2,12 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 const{register, login} = require('../controllers/userController')
-import { getAllUsers, getUserById, createUser } from '../controllers/userController'
+import { getAllUsers, getUserById } from '../controllers/userController'
 const{loginMiddleware} = require('../middleware/loginMiddleware')
 
 
 
 router.post('/register', register)
 router.post('/login', loginMiddleware, login )
+router.get('/', getAllUsers);
+router.get('/:id', getUserById);
 
 module.exports = router
