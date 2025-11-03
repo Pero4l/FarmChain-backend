c3f53e3dc60ee09371b1aa414a556b70bb979b37
pero4l <lilpero03@gmail.com>
2025-11-03 21:48:54 +0100

    feat:coverting project to my real farmchain project

--- models/users.js
+++ models/users.js
@@ -3,23 +3,52 @@ const connection = require("../config/db")
 
 const User =
  connection.define('newUser', {
-  id:{
-    type: DataTypes.INTEGER,
-    primaryKey:true,
-    autoIncrement:true
-  },
-
-  name:{
-    type: DataTypes.STRING,
-    allowNull:false
-  },
-  email:{
-    type: DataTypes.STRING,
-    allowNull:false 
-  }
+   id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    first_name: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    gender: {
+      type: DataTypes.ENUM('Male','Female','Unknown'),
+      allowNull: true,
+      defaultValue: "Unknown"
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    phone_no: {
+      type: DataTypes.STRING(20),
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING(20),
+      allowNull: false
+    },
+    country: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    }
 },
 {
-  tableName: "users2",
+  tableName: "users",
   timestamps: true
 }
 )
