7fb7effb189c4725039347bee9ec9bf65ce55444
pero4l <lilpero03@gmail.com>
2025-11-03 13:46:46 +0100

    feat: trying to create a db connection

--- config/db.js
+++ config/db.js
@@ -1,5 +1,8 @@
 const {sequelize, Sequelize} = require('sequelize')
+
 const connection = new Sequelize('farm_chain', 'sudo_ptb', 'p6565',{
     host: "localhost",
     dialect: "mysql"
 })
+
+module.exports = connection
