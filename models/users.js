8fc0dc344d5a0be801a9abfc6a5430fcab59bf50
pero4l <lilpero03@gmail.com>
2025-11-06 22:13:26 +0100

    feat:adding a relationship btw users and notification

--- models/users.js
+++ models/users.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Users.hasMany(models.Notifications, {foreignKey: 'user_id' });
     }
   }
   Users.init({
