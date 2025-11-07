50c5d7ae2681a65db8262efd80d45704cf39bdb5
pero4l <lilpero03@gmail.com>
2025-11-07 23:37:16 +0100

    feat: deleted the old dp_operatio

--- utils/dbOperation.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Replace these values with your MySQL credentials
-const sequelize = new Sequelize('test', 'root', '', {
-  host: 'localhost', // or your MySQL server IP
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
