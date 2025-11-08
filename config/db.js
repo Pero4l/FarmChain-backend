3a13971947aa5db5d734938c73f1797a90e69db2
pero4l <lilpero03@gmail.com>
2025-11-08 13:15:16 +0100

    feat: editted the config to freedb own

--- config/db.js
+++ config/db.js
@@ -1,10 +1,15 @@
-const {sequelize, Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+const connection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: console.log, // optional, useful to debug
+  }
+);
 
-const connection = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
-    host: "127.0.0.1",
-    dialect: "mysql"
-})
 module.exports = connection;
-
