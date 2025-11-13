f8ac6ffc81693fc00ba1e38cbd916bdbd3c7602a
pero4l <lilpero03@gmail.com>
2025-11-13 16:06:00 +0100

    feat:adujusted my connection to post grest

--- config/db.js
+++ config/db.js
@@ -1,9 +1,27 @@
-const {sequelize, Sequelize} = require('sequelize')
+// const {sequelize, Sequelize} = require('sequelize')
+// require('dotenv').config();
+
+
+// const connection = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
+//     host: "127.0.0.1",
+//     dialect: "mysql"
+// })
+// module.exports = connection;
+
+
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+const connection = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // Render Postgres requires SSL
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-const connection = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
-    host: "127.0.0.1",
-    dialect: "mysql"
-})
 module.exports = connection;
+
+
