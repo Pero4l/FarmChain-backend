9fed86d63bfd93e1ced1c80c42313bad25f53e1e
pero4l <lilpero03@gmail.com>
2025-11-27 00:50:15 +0100

    Reapply "feat: worked on the user profile post retrive and also updted my update profile"
    This reverts commit 180fbca9577dce15d43b607b7476e33366baa33d.

--- controllers/userController.js
+++ controllers/userController.js
@@ -143,26 +143,6 @@ async function login(req, res) {
   where: { user_id: user.userId }
 });
 
-  // Get followers/following counts
-    const followers = await Relationship.count({
-      where: { followed_id: user.userId }
-    });
-    const following = await Relationship.count({
-      where: { follower_id: user.userId }
-    });
-
-
-     // User posts count
-    const postsCount = await Posts.count({
-      where: { user_id: user.userId }
-    });
-
-    const posts = await Posts.findAll({
-      where: { user_id: user.userId },
-      // attributes: ['id', 'content', 'createdAt'],
-      order: [['createdAt', 'DESC']]
-    });
-
 
 const personalProfile = {
   name: user.currentUser,
@@ -172,10 +152,6 @@ const personalProfile = {
   bio: profile?.bio || "",
   organization: profile?.organization || "",
   verified: profile?.verified || false,
-  followers: followers || 0,
-  following: following || 0,
-  posts: posts,
-  totalPost: postsCount
   // share_account: profile?.share_account || ""
 };
 
