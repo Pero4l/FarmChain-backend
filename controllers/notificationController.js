d0842504d661f60c1223abe559d9ad517f666391
pero4l <lilpero03@gmail.com>
2025-11-30 21:31:11 +0100

    feat: writing the notification fn

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -2,4 +2,17 @@ const {Notifications} = require("../models");
 
 async function getNotification(req, res){
 
-}
+    const id = req.user?.userId;
+
+    const notification = await Notifications.findAll({
+        where: {user_id: id},
+        order: [["createdAt", "DESC"]]
+    });
+
+    res.status(200).json({
+        "success": true,
+        "message": "Gotten users notification successfully",
+        "data": notification
+    })
+};
+
