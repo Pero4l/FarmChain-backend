31bd8513d890055b244b0f8477d6d0ff24220261
pero4l <lilpero03@gmail.com>
2025-12-05 12:40:00 +0100

    feat: I changed my database

--- config/db.js
+++ config/db.js
@@ -8,19 +8,52 @@
 // })
 // module.exports = connection;
 
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-const connection = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const connection = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//       require: true, 
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
+
+// module.exports = connection;
+
+
+
+
+
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+
+const connection = new Sequelize({
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host:  process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
   dialectOptions: {
     ssl: {
-      require: true, // Render Postgres requires SSL
-      rejectUnauthorized: false,
+      require: true,
+      rejectUnauthorized: false, 
+      ca: fs.readFileSync(path.join(__dirname, "certs/ca.pem")).toString(),
     },
   },
 });
 
 module.exports = connection;
+
+
+
+
 
 
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -34,3 +34,30 @@ async function sendOTPEmail(email, otp) {
 }
 
 module.exports = { sendOTPEmail };
+
+// require("dotenv").config();
+// const { Resend } = require("resend");
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// async function sendOTPEmail(email, otp) {
+//   try {
+//     await resend.emails.send({
+//       from: "FarmChain <farmchaininfo@gmail.com>",
+//       to: email,
+//       subject: "Your FarmChain OTP Code",
+//       html: `
+//         <h2>FarmChain OTP Verification</h2>
+//         <p>Your 4-digit OTP is:</p>
+//         <h1 style="font-size: 32px; letter-spacing: 6px;">${otp}</h1>
+//         <p>This code expires in <b>5 minutes</b>.</p>
+//       `,
+//     });
+
+//     console.log("OTP sent successfully using Resend");
+//   } catch (error) {
+//     console.error("RESEND ERROR:", error);
+//     throw error;
+//   }
+// }
+
+// module.exports = { sendOTPEmail };
