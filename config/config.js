4d1dcc105624f173e2a740fb419c27739e68aa85
pero4l <lilpero03@gmail.com>
2025-12-05 15:21:18 +0100

    feat: I adjusted my db setup

--- config/config.js
+++ config/config.js
@@ -25,7 +25,6 @@
 // }
 
 
-
 require('dotenv').config();
 
 module.exports = {
@@ -54,3 +53,39 @@ module.exports = {
     },
   },
 };
+
+
+// require('dotenv').config();
+
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//       },
+//       sslmode: "require"
+//     }
+//   },
+//   production: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//       },
+//       sslmode: "require"
+//     }
+//   }
+// }
--- config/db.js
+++ config/db.js
@@ -9,16 +9,43 @@
 // module.exports = connection;
 
 
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+
+require('dotenv').config();
+
+const { Sequelize } = require('sequelize');
+
+
+const connection = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = connection;
+
+
 
 // const { Sequelize } = require('sequelize');
 // require('dotenv').config();
+
+
+// const fs = require('fs');
+// const path = require('path');
 
 // const connection = new Sequelize(process.env.DATABASE_URL, {
 //   dialect: 'postgres',
 //   dialectOptions: {
 //     ssl: {
-//       require: true, 
-//       rejectUnauthorized: false,
+//       require: true,
+//       rejectUnauthorized: true,
+//       ca: fs.readFileSync(path.join(__dirname, 'certs/ca.pem')).toString(),
 //     },
 //   },
 // });
@@ -27,30 +54,6 @@
 
 
 
-
-
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const fs = require("fs");
-const path = require("path");
-
-const connection = new Sequelize({
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host:  process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, 
-      ca: fs.readFileSync(path.join(__dirname, "certs/ca.pem")).toString(),
-    },
-  },
-});
-
-module.exports = connection;
 
 
 
